296e7c7f535bddc16c131675ea98dd5049bdd6e3
Rene Ramirez <rene.ramirez@verlas.com.ve>
2015-06-27 17:15:10 -0400

    Adds the header and the video

--- src/FrontendBundle/Controller/DefaultController.php
+++ src/FrontendBundle/Controller/DefaultController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller {
 
   public function indexAction() {
-    return $this->render('FrontendBundle:Default:index.html.twig', array());
+    return $this->render('FrontendBundle:Page:index.html.twig', array());
   }
 
 }
